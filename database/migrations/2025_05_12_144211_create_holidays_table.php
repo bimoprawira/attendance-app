ba17af20f205a3362e1edf64354f21b7644b1b9f
mystlicious <akmag2007@gmail.com>
2025-05-12 22:05:12 +0700

    Fixed migration holiday

--- database/migrations/2025_05_12_144211_create_holidays_table.php
+++ database/migrations/2025_05_12_144211_create_holidays_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('holidays', function (Blueprint $table) {
             $table->id();
+            $table->date('date')->unique();
+            $table->string('name');
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
